0db89ae7e60364465104bb0615f67583244d2232
tdwillson <86855463+tdwillson@users.noreply.github.com>
2021-11-03 14:54:13 -0400

    Added SQL connection to save cover letter entries

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import render_template, Flask, request
+import sqlite3
 
 app = Flask(__name__)
 
@@ -15,4 +16,8 @@ def index():
         if not position:
             return "No position entered"
         addon = request.form.get("addon")
+        db = sqlite3.connect("letter.db")
+        c = db.cursor()
+        c.execute(f"INSERT INTO letters (company, position, addon) VALUES ('{company}','{position}','{addon}'")
+        c.commit()
         return render_template("coverletter.html", company=company, position=position, addon=addon)
